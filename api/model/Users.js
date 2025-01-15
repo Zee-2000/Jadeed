31331cf6aa16d49ec5c56de7466f8700aa15310b
Zee-2000 <zahwaabdelnasser@gmail.com>
2025-01-15 13:23:43 +0200

    test

--- api/model/Users.js
+++ api/model/Users.js
@@ -6,20 +6,17 @@ const userSchema = mongoose.Schema(
         name: { type: String, required: true },
         age: { type: Number, required: true },
         email: { type: String, required: true },
-        password:{type: String, required:true},
-        isAdmin: {type: Boolean, default:false},
+        password: { type: String, required: true },
+        isAdmin: { type: Boolean, default: false },
     }
 );
 
-userSchema.methods.matchPassword = async function(enterPassword)
-{
+userSchema.methods.matchPassword = async function (enterPassword) {
     return await bcrypt.compare(enterPassword, this.password)
 }
 //register encryption
-userSchema.pre("save", async function(next)
-{
-    if(!this.password.isModified(password))
-    {
+userSchema.pre("save", async function (next) {
+    if (!this.password.isModified(password)) {
         next();
     }
 
