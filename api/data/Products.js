db04d9d1bbc972aa6a421da270e4ca899bf25707
Zee-2000 <zahwaabdelnasser@gmail.com>
2025-01-15 12:50:19 +0200

    generating salt, updating and fixing products schema

--- api/data/Products.js
+++ api/data/Products.js
@@ -2,38 +2,43 @@ const products =
 [
   {
   
-  "name" : "Aller Aqua fish feed",
-  "price" : "200 L.E",
-  "description" :"This fish feed is from Aller Aqua Company with 30% protein",
-  "rating" : "5",
-  "countInStock" : "150"
+  name : "Aller Aqua fish feed",
+  price : "200 L.E",
+  description :"This fish feed is from Aller Aqua Company with 30% protein",
+  rating : "5",
+  countInStock : "150"
 
 }, 
 {
   
-  "name" : "Canal AquaCure KMnO4",
-  "price" : "1000 L.E",
-  "description" :"Potassium Permenganate for Tank and pond Quarantine",
-  "rating" : "9",
-  "countInStock" : "10"
+  name : "Canal AquaCure KMnO4",
+  price : "1000 L.E",
+  description :"Potassium Permenganate for Tank and pond Quarantine",
+  rating : "9",
+  countInStock : "10"
 },
  {
   
-  "name" : "Fish tank",
-  "price" : "900 L.E",
-  "description" :"With size 20 x 20 fits fingerlings",
-  "rating" : "10",
-  "countInStock" : "1050"
+  name : "Fish tank",
+  price : "900 L.E",
+  description :"With size 20 x 20 fits fingerlings",
+  rating : "10",
+  countInStock : "1050"
 },
  {
   
-  "name" : "Testosterne Hormone",
-  "price" : "2000 L.E",
-  "description" :"For better fertilityto fish in your farm"
+  name : "Testosterne Hormone",
+  price : "2000 L.E",
+  description :"For better fertilityto fish in your farm",
+  rating : "30",
+  countInStock : "230"
 }, 
 {
   
-  "name" : "Trammel Nets",
-  "price" : "800 L.E",
-  "description" :"To catch the fish in your farm , Length 30 x 30, can catch big fish only"
+  name : "Trammel Nets",
+  price : "800 L.E",
+  description :"To catch the fish in your farm , Length 30 x 30, can catch big fish only",
+  rating : "40",
+  countInStock : "890"
 }]
+module.exports = products;
--- api/model/Users.js
+++ api/model/Users.js
@@ -15,4 +15,14 @@ userSchema.methods.matchPassword = async function(enterPassword)
 {
     return await bcrypt.compare(enterPassword, this.password)
 }
+userSchema.pre("save", async function(next)
+{
+    if(!this.password.isModified(password))
+    {
+        next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
 module.exports = mongoose.model("User", userSchema);
