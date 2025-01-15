626c4780055456833b2f9f1165318255c263a2c4
Zee-2000 <zahwaabdelnasser@gmail.com>
2025-01-15 12:07:23 +0200

    Create register  route

--- api/routes/User.js
+++ api/routes/User.js
@@ -2,25 +2,51 @@ const userRoute = require('express').Router();
 const AsyncHandler = require('express-async-handler');
 const User = require('../model/Users')
 
-userRoute.post("/login", 
-    AsyncHandler(async(req, res)=> {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
-    if (user && (await user.matchPassword(password))) {
-        res.json(
-            {
-                _id: user.id,
-                name: user.name,
-                isAdmin: user.isAdmin,
-                token: null,
-                createdAt: user.createdAt
+userRoute.post("/login",
+    AsyncHandler(async (req, res) => {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (user && (await user.matchPassword(password))) {
+            res.json(
+                {
+                    _id: user.id,
+                    name: user.name,
+                    isAdmin: user.isAdmin,
+                    token: null,
+                    createdAt: user.createdAt
+                }
+            )
+        }
+        else {
+            res.status(401);
+            throw new Error("Invalid Email or password, Try Again!");
+        }
+    })
+);
+
+userRoute.post("/",
+    AsyncHandler(async (req, res) => {
+        const { name, age, email, password } = req.body;
+        const userExist = await User.findOne({ email });
+        if (userExist) {
+            res.status(400);
+            throw new Error("User Already exists");
+        }
+        else {
+            const user = await User.create({ name, age, email, password });
+            if (user) {
+                res.status(201).json(
+                    {
+                        _id: user.id,
+                        name: user.name,
+                        isAdmin: user.isAdmin,
+                        token: null,
+                        createdAt: user.createdAt
+                    }
+                )
             }
-        )
-    }
-    else {
-        res.status(401);
-        throw new Error("Invalid Email or password, Try Again!");
-    }
-})
+        }
+
+    })
 );
 module.exports = userRoute;
