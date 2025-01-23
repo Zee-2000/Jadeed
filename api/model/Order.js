62a13a7ce41ee29578d3b7ca1cb7f1a3b7fdb4cf
Zee-2000 <zahwaabdelnasser@gmail.com>
2025-01-23 21:54:21 +0200

    Adding payment feature

--- api/model/Order.js
+++ api/model/Order.js
@@ -1,42 +1,68 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const orderItemsSchema = new mongoose.Schema({
-    name: { type: String, required: true, default: 0 },
-    price: { type: Number, required: true, default: 0 },
-    quantity: { type: Number, required: true, default: 0 },
-    image: { type: String, required: true, default: 0 },
-    products: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "Products" },
+const orderItemSchema =  mongoose.Schema({
+  name: { type: String, required: true },
+  qty: { type: Number, required: true },
+  image: { type: String, required: true },
+  price: { type: Number, required: true },
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+    required: true,
+  },
 });
 
 const orderSchema = new mongoose.Schema(
-    {
-        user: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
-        orderItems: [orderItemsSchema],
-        shippingAddress: {
-            address: { type: String, required: true },
-            postalCode: { type: String, required: true },
-            city: { type: String, required: true },
-            country: { type: String, required: true },
-        },
-        paymentMethod: { type: String, required: true, default: "PayPal" },
-
-        paymentResult: {
-            id: { type: String },
-            status: { type: String },
-            emailAddress: { type: String },
-            updatedTime: { type: String },
-        },
-        taxPrice: { type: Number, requied: true, default: 0.0 },
-        shippingPrice: { type: Number, required: true, deafult: 0.0 },
-        totalPrice: { type: Number, required: true, default: 0.0 },
-        isPaid: { type: Boolean, required: true, default: false },
-        paidAt: { type: Date },
-        isDelivered: { type: Boolean, required: true, default: false },
-        deliveredAt: { type: Date, required: true, default: false },
-        refund: { type: Number, required: true, default: false },
-        refundDate: { type: Date, required: true, deafult: false },
-        tiemstamps:{createdAt: 'createdAt', updatedAt : 'updatedAt', required : true},
-    }
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
+    orderItems: [orderItemSchema],
+    shippingAddress: {
+      address: { type: String, required: true },
+      city: { type: String, required: true },
+      postalCode: { type: String, required: true },
+      country: { type: String, required: true },
+    },
+    paymentMethod: { type: String, required: true, default: "Paypal" },
+    paymentResult: {
+      id: { type: String },
+      status: { type: String },
+      updated_time: { type: String },
+      email_address: { type: String },
+    },
+    taxPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+    shippingPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+    totalPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+    isPaid: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    paidAt: {
+      type: Date,
+    },
+    isDelivered: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    deliveredAt: {
+      type: Date,
+    },
+  },
+  { timestamps: true }
 );
 
-module.exports = mongoose.model(orderSchema, "Order");
+
+module.exports = mongoose.model("Order", orderSchema)
--- api/routes/Order.js
+++ api/routes/Order.js
@@ -1,7 +1,7 @@
 const orderRoute = require("express").Router();
 const AsyncHandler = require("express-async-handler");
 const authentication = require("../middleware/Auth");
-
+const Order = require("../model/Order")
 
 
 orderRoute.post("/order",
@@ -36,4 +36,59 @@ orderRoute.post("/order",
             res.status(201).json(createdOrder);
         }
     }));
+
+//Order details
+orderRoute.get("/:id",
+    authentication,
+    AsyncHandler(async (req, res) => {
+        const order = await Order.findById(req.params.id).populate(
+            "user",
+            "name email"
+        );
+        if (order) {
+            res.status(201).json(order);
+        }
+        else {
+            res.status(401);
+            throw new Error("Order not found")
+        }
+    }));
+
+//Order Payment
+orderRoute.put("/:id/payment",
+    authentication,
+    AsyncHandler(async (req, res) => {
+        const order = await Order.findById(req.params.id);
+        if (order) {
+            isPaid = true;
+            order.paidAt = Date.now()
+            order.paymentResult =
+            {
+                id: req.body.id,
+                status: req.body.status,
+                emailAddress: req.body.emailAddress,
+                updatedTime: req.body.updatedTime
+            }
+            const createdOrder = await order.save();
+            res.status(201).json(createdOrder);
+        }
+        else {
+            res.status(404);
+            throw new Error("No Orders found!")
+        }
+    }));
+//Order List
+orderRoute.get("/",
+    authentication,
+    AsyncHandler(async (req, res) => {
+        const orders = await Order.find({ user: req.user.id }).sort({ _id: -1 });
+        if (orders) {
+            res.status(200).json(order);
+        }
+        else {
+            res.status(404);
+            throw new Error("No Orders is listed")
+        }
+    }
+    ));
 module.exports = orderRoute;
