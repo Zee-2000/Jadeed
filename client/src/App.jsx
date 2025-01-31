acd370d02f4212aad0caa0bdc2696aa8a1d0fbf1
Zee-2000 <zahwaabdelnasser@gmail.com>
2025-01-31 22:28:16 +0200

    Final touch

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,109 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Layout from './Layout'
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import ProductDetail from "./pages/ProductDetail";
+import Home from "./pages/Home";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Checkout from "./pages/Checkout";
+import { useSelector } from "react-redux";
+import PlaceOrder from "./pages/PlaceOrder";
+import OrderConfirmation from "./pages/OrderConfirm";
+import { OrderHistory } from "./pages/OrderHistory";
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const userLoginReducer = useSelector((state) => state.userLoginReducer)
+  const {userInfo} = userLoginReducer
   return (
     <>
-      <Layout>
-        <section class="text-gray-600 body-font">
-          <div class="container px-5 py-24 mx-auto">
-            <div class="flex flex-col text-center w-full mb-20">
-              <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Our Team</h1>
-              <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them.</p>
-            </div>
-            <div class="flex flex-wrap -m-2">
-              <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
-                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                  <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/80x80" />
-                  <div class="flex-grow">
-                    <h2 class="text-gray-900 title-font font-medium">Holden Caulfield</h2>
-                    <p class="text-gray-500">UI Designer</p>
-                  </div>
-                </div>
-              </div>
-              <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
-                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                  <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/84x84" />
-                  <div class="flex-grow">
-                    <h2 class="text-gray-900 title-font font-medium">Henry Letham</h2>
-                    <p class="text-gray-500">CTO</p>
-                  </div>
-                </div>
-              </div>
-              <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
-                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                  <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/88x88" />
-                  <div class="flex-grow">
-                    <h2 class="text-gray-900 title-font font-medium">Oskar Blinde</h2>
-                    <p class="text-gray-500">Founder</p>
-                  </div>
-                </div>
-              </div>
-              <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
-                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                  <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/90x90" />
-                  <div class="flex-grow">
-                    <h2 class="text-gray-900 title-font font-medium">John Doe</h2>
-                    <p class="text-gray-500">DevOps</p>
-                  </div>
-                </div>
-              </div>
-              <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
-                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                  <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/94x94" />
-                  <div class="flex-grow">
-                    <h2 class="text-gray-900 title-font font-medium">Martin Eden</h2>
-                    <p class="text-gray-500">Software Engineer</p>
-                  </div>
-                </div>
-              </div>
-              <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
-                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                  <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/98x98" />
-                  <div class="flex-grow">
-                    <h2 class="text-gray-900 title-font font-medium">Boris Kitua</h2>
-                    <p class="text-gray-500">UX Researcher</p>
-                  </div>
-                </div>
-              </div>
-              <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
-                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                  <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/100x90" />
-                  <div class="flex-grow">
-                    <h2 class="text-gray-900 title-font font-medium">Atticus Finch</h2>
-                    <p class="text-gray-500">QA Engineer</p>
-                  </div>
-                </div>
-              </div>
-              <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
-                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                  <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/104x94" />
-                  <div class="flex-grow">
-                    <h2 class="text-gray-900 title-font font-medium">Alper Kamu</h2>
-                    <p class="text-gray-500">System</p>
-                  </div>
-                </div>
-              </div>
-              <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
-                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                  <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/108x98" />
-                  <div class="flex-grow">
-                    <h2 class="text-gray-900 title-font font-medium">Rodrigo Monchi</h2>
-                    <p class="text-gray-500">Product Manager</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-      </Layout>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/products/:id" element={<ProductDetail />}></Route>
+          <Route
+            exact
+            path="/login"
+            element={userInfo ? <Navigate to="/"></Navigate> : <Login />}
+          ></Route>
+          <Route
+            exact
+            path="/register"
+            element={userInfo ? <Navigate to="/"></Navigate> : <Register />}
+          ></Route>
+          <Route path="/order/:id" element={<OrderConfirmation />} />
+          <Route path="/order-history" element={<OrderHistory />} />
+
+          <Route exact path="/checkout" element={<Checkout />}></Route>
+          <Route exact path="/placeorder" element={<PlaceOrder />}></Route>
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
