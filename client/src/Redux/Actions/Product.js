4fb350a5361b996ec7f15b4785903b2f49e88e25
Zee-2000 <zahwaabdelnasser@gmail.com>
2025-02-04 10:01:51 +0200

    fixing       product action

--- client/src/Redux/Actions/Product.js
+++ client/src/Redux/Actions/Product.js
@@ -1,46 +1,44 @@
-import axios from "axios";
+import axios from "axios"
 import {
     PRODUCT_LIST_REQ,
     PRODUCT_LIST_REQ_SUCCESS,
-    PRODUCT_LIST_REQ_FAIL, 
+    PRODUCT_LIST_REQ_FAIL,
+
     PRODUCT_DETAIL_REQ,
     PRODUCT_DETAIL_REQ_SUCCESS,
     PRODUCT_DETAIL_REQ_FAIL
-}from '/src/Redux/Contents/Product.js';
-import {BASE_URL} from "../Contents/BASE_URL";
+} from "../Contents/Product"
 
-export const productListAction = ()=>async(dispatch)=>
+import {BASE_URL} from "../Contents/BASE_URL"
 
-{
-    try
-    {
-        dispatch({type : PRODUCT_LIST_REQ});
+export const productListAction = () => async (dispatch) => {
+    try {
+        dispatch({ type: PRODUCT_LIST_REQ });
         const { data } = await axios.get(`${BASE_URL}/api/products`);
-
-    dispatch({type : PRODUCT_LIST_REQ_SUCCESS, payload : data});
-    }catch
-    {
-        dispatch({type : PRODUCT_LIST_REQ_FAIL, payload : error.response && error.response.data.message ? error.response.data.message : error.message});
+        console.log(data);
+        dispatch({ type: PRODUCT_LIST_REQ_SUCCESS, payload: data })
+    } catch (error) {
+        dispatch({
+            type: PRODUCT_LIST_REQ_FAIL,
+            payload: error.response && error.response.data.message ? error.response.data.message : error.message
+        })
     }
     
-
-
 }
 
-export const productAction = (id)=> async(dispatch)=>
 
-    {
-        try
-        {
-            dispatch({type : PRODUCT_DETAIL_REQ});
-        const{data} = await axios.get(`${BASE_URL}/api/products/${id}`);
-    
-        dispatch({type : PRODUCT_DETAIL_REQ_SUCCESS, payload : data});
-        }catch
-        {
-            dispatch({type : PRODUCT_DETAIL_REQ_FAIL, payload : error.response && error.response.data.message ? error.response.data.message : error.message});
-        }
-        
-    
-    
+
+
+export const productAction = (id) => async (dispatch) => {
+    try {
+        dispatch({ type: PRODUCT_DETAIL_REQ });
+        const { data } = await axios.get(`${BASE_URL}/api/products/${id}`);
+        dispatch({ type: PRODUCT_DETAIL_REQ_SUCCESS, payload: data })
+    } catch (error) {
+        dispatch({
+            type: PRODUCT_DETAIL_REQ_FAIL,
+            payload: error.response && error.response.data.message ? error.response.data.message : error.message
+        })
     }
+
+}
