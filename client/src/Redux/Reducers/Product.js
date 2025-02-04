2ba386c39054dddfeed886d711b8e54b33d0b524
Zee-2000 <zahwaabdelnasser@gmail.com>
2025-02-04 09:35:27 +0200

    fixing product reducer

--- client/src/Redux/Reducers/Product.js
+++ client/src/Redux/Reducers/Product.js
@@ -2,38 +2,47 @@ import {
     PRODUCT_LIST_REQ,
     PRODUCT_LIST_REQ_SUCCESS,
     PRODUCT_LIST_REQ_FAIL,
+
     PRODUCT_DETAIL_REQ,
     PRODUCT_DETAIL_REQ_SUCCESS,
     PRODUCT_DETAIL_REQ_FAIL
-} from "../Contents/Product";
-//for product list
+
+} from "../Contents/Product"
+
+//list of products
 export const productListReducer = (state = { products: [] }, action) => {
     switch (action.type) {
-        case (PRODUCT_LIST_REQ):
-            return({
-            loading: true, products : []
-        })
-        case (PRODUCT_LIST_REQ_SUCCESS):
-            return ({loading : false , products : action.payload.products, totalPages : action.payload.totalPages, page : action.payload.page});
-        case (PRODUCT_LIST_REQ_FAIL):
-            return ({ loading: false, error: action.payload.error })
+        case PRODUCT_LIST_REQ:
+            return {
+                loading: true, products: [] , 
+                
+            };
+        case PRODUCT_LIST_REQ_SUCCESS:
+            return {
+                loading:false, products: action.payload, totalPage: action.payload.totalPage, page:action.payload.page
+            }
+        case PRODUCT_LIST_REQ_FAIL:
+            return { loading: false, error: action.payload.error }
         default:
-            return state;
+            return state
     }
 }
 
-//for single products
-export const productReducer = (state = { product: {reviews :[]} }, action) => {
+// single product by id
+export const productReducer = (state = { product: {reviews:[]} }, action) => {
     switch (action.type) {
-        case (PRODUCT_DETAIL_REQ):
-            return({
-            loading: true, ...state
-        })
-        case (PRODUCT_DETAIL_REQ_SUCCESS):
-            return ({loading : false , product : action.payload});
-        case (PRODUCT_DETAIL_REQ_FAIL):
-            return ({ loading: false, error: action.payload })
+        case PRODUCT_DETAIL_REQ:
+            return {
+                loading: true, ...state
+            };
+        case PRODUCT_DETAIL_REQ_SUCCESS:
+            return {
+                loading: false, product: action.payload
+            }
+        case PRODUCT_DETAIL_REQ_FAIL:
+            return { loading: false, error: action.payload }
         default:
-            return state;
+            return state
     }
 }
+
