70e571412391d86becefc5dd22230c66b45fb686
Zee-2000 <zahwaabdelnasser@gmail.com>
2025-02-04 10:23:47 +0200

    install reduxjs/toolkit

--- client/src/Redux/store.js
+++ client/src/Redux/store.js
@@ -1,11 +1,20 @@
 import { combineReducers } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import {storage} from "redux-persist/lib/storage";
 import {persistStore, persistReducer} from "redux-persist";
 import { version } from "react";
+import { productListReducer, productReducer } from "./Reducers/Product";
 const persistConfig = {
   key : "root",
   storage,
   version : "1.0"
 }
 
-const rootReducer = combineReducers
+const rootReducer = combineReducers(
+  productListReducer,
+  productReducer
+);
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+export const store = configureStore(persistedReducer);
+export let persistor = persistStore(store);
